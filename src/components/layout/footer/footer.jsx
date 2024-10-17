d8e06fd59e88fd78dc491c7a810f5ae768c83ea4
RockyBalboa21 <aliaxandr.gerasimau.@gmail.com>
2024-10-17 18:36:16 +0300

    Шаг 4. Вёрстка layout и шлифовка структуры проета, добавление логотипа

--- src/components/layout/footer/footer.jsx
+++ src/components/layout/footer/footer.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import Logo from "/src/components/ui/logo/logo";
+import "./style.css";
 
 function Footer() {
-  return <></>;
+  return (
+    <footer className="footer">
+      <Logo />
+      <span className="footer__copyright"> Создано 2021</span>
+    </footer>
+  );
 }
 
 export default Footer;
+
--- src/components/layout/page-wrapper/page-wrapper.jsx
+++ src/components/layout/page-wrapper/page-wrapper.jsx
@@ -1,8 +1,23 @@
 import React from "react";
+import Header from "/src/components/layout/header/header";
+import Footer from "/src/components/layout/footer/footer";
+import MainPage from "/src/components/pages/main-page/main-page";
+import "./style.css";
 
 // Обёртка для контента страниц
 function PageWrapper() {
-  return <></>;
+  return (
+    <>
+      <Header />
+      <main className="page-wrapper__main">
+        <MainPage />
+        <h3>Вёрстка layout</h3>
+
+        <div>По аналогии можно создать и написать стили для всех компонентов директории layout, а также использующихся в layout компонентов Button и Logo. В результате, если подключить PageWrapper в App, то будет выведена страница с шапкой и подвалом, но пока без контента.</div>
+      </main>
+      <Footer />
+    </>
+  );
 }
 
 export default PageWrapper;
