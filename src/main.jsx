3cee890e4b03299d05ead1c7d8329e894c63f9a1
RockyBalboa21 <aliaxandr.gerasimau.@gmail.com>
2024-10-25 22:51:28 +0300

    Cтилизуем главную страницу, используя Styled components Через терминал установил зависимости
    npm i styled-components
    
    Описал тему приложения и глобальные стили

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,14 @@
 import { StrictMode } from 'react'
+import { ThemeProvider } from "styled-components";
+import { defaultTheme } from "/src/thems/default.js";
 import { createRoot } from 'react-dom/client'
 import App from './components/app/App'
 
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <ThemeProvider theme={defaultTheme}>
+      <App />
+    </ThemeProvider>
   </StrictMode>,
 )
