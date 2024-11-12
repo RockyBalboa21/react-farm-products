5cd0db8121a62597065fbff5407abc06604ac5aa
RockyBalboa21 <aliaxandr.gerasimau.@gmail.com>
2024-11-13 01:16:32 +0300

    Шаг 2. Разметка страницы Страница оформления заказа состоит из двух колонок: левой и правой. В левой колонке вверху располагается панель с выбором продуктов, а внизу — панель для оформления заказа. В правой колонке находится слайдер, в котором каждый слайд является детальным описанием продукта.
    Наметим эти элементы в компоненте Order.
    Чтобы сделать Order было легче, мы добавили несколько дополнительных компонентов.
    
    1. Panel — добавляет стили к Section так, чтобы она выглядела как панель в макете.
    
    2. Компонент TextInput стилизует элемент ввода.
    
    3. Компонент Label стилизует HTML-элемент label.

--- src/components/pages/order/order.jsx
+++ src/components/pages/order/order.jsx
@@ -1,9 +1,42 @@
+import Panel from "/src/components/ui/panel/panel";
+import Title, { TitleSize } from "/src/components/ui/title/title";
+import Button from "/src/components/ui/button/button";
+import {
+    LeftColumn,
+    StyledOrder,
+    AddressInput,
+    PriceLabel,
+    PriceValue,
+} from "./styles";
+
+// Оформление заказа
 function Order() {
-	return(
-		<>
-			<div>Order</div>
-		</>
-	);
+    return (
+        <StyledOrder as="form">
+            <LeftColumn>
+                <Panel $marginBottom={20} $paddingTop={24} $paddingBottom={10}>
+                    <Title
+                        as="h2"
+                        size={TitleSize.EXTRA_SMALL}
+                        $marginBottom={12}
+                    >
+                        Выберите продукты
+                    </Title>
+                    Чекбокс со списком продуктов
+                </Panel>
+                <Panel>
+                    <Title size={TitleSize.EXTRA_SMALL} $marginBottom={24}>
+                        Сделать заказ
+                    </Title>
+                    <AddressInput placeholder="Введите адрес доставки" />
+                    <PriceLabel as="span">Цена</PriceLabel>
+                    <PriceValue>400</PriceValue>
+                    <Button maxWidth>Купить</Button>
+                </Panel>
+            </LeftColumn>
+            <div>Сюда нужно добавить слайдер с продуктами</div>
+        </StyledOrder>
+    );
 }
 
 export default Order;
