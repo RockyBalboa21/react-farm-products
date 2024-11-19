103e73ae5ca750d9651b53f256010f72ebb15120
RockyBalboa21 <aliaxandr.gerasimau.@gmail.com>
2024-11-19 14:10:28 +0300

    Шаг 4. Рефакторинг страниц
    Роутинг работает, но пока на всех страницах выводится форма покупки. Так происходит, потому что в разметке PageWrapper жёстко забита страница покупки.
    
    Поменяем PageWrapper так, чтобы он стал общим шаблонам для всех страниц и получал отображаемую страницу через Outlet. А затем добавим его в App.
    
    В результате PageWrapper примет вид
    
    В App появятся компоненты страниц, разные для каждого роута

--- src/components/layout/page-wrapper/page-wrapper.jsx
+++ src/components/layout/page-wrapper/page-wrapper.jsx
@@ -1,34 +1,45 @@
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 import Header from "/src/components/layout/header/header";
 import Footer from "/src/components/layout/footer/footer";
-import Order from "/src/components/pages/order/order";
+import { Outlet } from "react-router-dom";
 import { Main } from "./styles";
 
 // Обёртка для контента страниц
-function PageWrapper({ ...prop }) {
-    return (
-        <>
-            <Header />
-            <Main>
-                <Order {...prop} />
-            </Main>
-            <Footer />
-        </>
-    );
+function PageWrapper({ products }) {
+	return (
+		<>
+			<Header />
+			<Main>
+				<Outlet />
+			</Main>
+			<Footer />
+		</>
+	);
 }
 
 PageWrapper.propTypes = {
-    children: PropTypes.node,
-    features: PropTypes.arrayOf(
-        PropTypes.shape({
-            id: PropTypes.number.isRequired,
-            title: PropTypes.string.isRequired,
-            owner: PropTypes.string.isRequired,
-            isNegative: PropTypes.bool.isRequired,
-            image: PropTypes.string.isRequired,
-            about: PropTypes.string.isRequired,
-        })
-    ).isRequired,
+	products: PropTypes.arrayOf(
+		PropTypes.shape({
+			id: PropTypes.number.isRequired,
+			name: PropTypes.string.isRequired,
+			description: PropTypes.string.isRequired,
+			price: PropTypes.number.isRequired,
+			weight: PropTypes.number.isRequired,
+			image: PropTypes.string.isRequired,
+			specifications: PropTypes.arrayOf(
+				PropTypes.shape({
+					property: PropTypes.string.isRequired,
+					value: PropTypes.string.isRequired,
+				})
+			).isRequired,
+			structure: PropTypes.arrayOf(
+				PropTypes.shape({
+					property: PropTypes.string.isRequired,
+					value: PropTypes.string.isRequired,
+				})
+			).isRequired,
+		})
+	).isRequired,
 };
 
 export default PageWrapper;
